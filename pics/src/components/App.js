fd93445cccadb65189b085193504c374562bc039
cristiaan3003 <cristiaan3003@gmail.com>
2020-04-30 09:11:43 -0300

    misc ... before init code

---
+++ pics/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+const  App = () => {
+    return (
+            <React.Fragment>
+                App
+            </React.Fragment>
+        );
+}
+
+export default App;
