da5f83196a1081301549913231cbfa8f3847847d
cristiaan3003 <cristiaan3003@gmail.com>
2020-04-30 14:24:55 -0300

    v.93 handlig async request - first form

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 
 import SearchBar from './SearchBar'
 
@@ -7,7 +8,15 @@ import SearchBar from './SearchBar'
 class  App extends React.Component {
 
     onSearchSubmit(term){
-            console.log(term);
+        axios.get('https://api.unsplash.com/search/photos',{
+            params: {query: term},
+            headers: {
+                Authorization: 'Client-ID fE32I_b5LZuwgt5FYHoxD7JgYVm_cij9wxB_HFcsuDk'
+            }
+
+        }).then( (response)=>{
+            console.log(response.data.results)
+        });
     }
 
     render () {
