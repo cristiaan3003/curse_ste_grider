2054bdf5788b56f734d4155d97ded147316b5908
cristiaan3003 <cristiaan3003@gmail.com>
2020-04-30 14:26:56 -0300

    v.93 handlig async request - second form

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -7,16 +7,15 @@ import SearchBar from './SearchBar'
 
 class  App extends React.Component {
 
-    onSearchSubmit(term){
-        axios.get('https://api.unsplash.com/search/photos',{
+    async onSearchSubmit(term){
+        const response = await axios.get('https://api.unsplash.com/search/photos',{
             params: {query: term},
             headers: {
                 Authorization: 'Client-ID fE32I_b5LZuwgt5FYHoxD7JgYVm_cij9wxB_HFcsuDk'
             }
 
-        }).then( (response)=>{
-            console.log(response.data.results)
         });
+        console.log(response.data.results);
     }
 
     render () {
