635c76e1c5c3e05df9bf0d3e72afa00a8f883280
cristiaan3003 <cristiaan3003@gmail.com>
2020-05-05 14:08:13 -0300

    v.143 - songsAPP - first commit

---
+++ songs_144/src/reducers/index.js
@@ -0,0 +1,18 @@
+const songReducer = () =>{
+    return [
+        {title: 'a', duration: '4:05'},
+        {title: 'b', duration: '4:06'},
+        {title: 'c', duration: '4:07'},
+        {title: 'd', duration: '4:08'},
+    ];
+};
+
+
+const selectedSongReducer = (selectedSong = null, action) =>{
+    if (action.type==='SONG_SELECTED'){
+        return action.payload;
+    }
+
+    return selectedSong;
+}
+
