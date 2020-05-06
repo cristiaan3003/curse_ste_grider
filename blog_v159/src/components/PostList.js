7b0b7bb92dde823209122502926bafc705d31674
cristiaan3003 <cristiaan3003@gmail.com>
2020-05-06 12:37:56 -0300

    v.159-163 - blogAPP - firts commit

---
+++ blog_v159/src/components/PostList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PostList extends React.Component{
+
+    render(){
+        return (
+        <div>
+
+            PostList
+        </div>
+        );
+    }
+}
+
+export default PostList;
