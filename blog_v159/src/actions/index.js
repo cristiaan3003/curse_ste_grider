86e1c479769a656c20b3996f68beeec0dc0209c2
cristiaan3003 <cristiaan3003@gmail.com>
2020-05-08 11:00:50 -0300

    misc

--- blog_v159/src/actions/index.js
+++ blog_v159/src/actions/index.js
@@ -16,7 +16,7 @@ export const fetchPosts = () => async dispatch => {
     dispatch({type: 'FETCH_POSTS' , payload: response});
 };
 
-export const fetchUser = (id) => async dispatch =>_fetchUser(id, dispatch);
+export const fetchUser = id => dispatch =>_fetchUser(id, dispatch);
 const _fetchUser = _.memoize ( async  (id, dispatch ) =>{
     const response = await jsonPlaceHolder.get(`/users/${id}`); //get('/users/'+id); 
     dispatch({type: 'FETCH_USER' , payload: response});
