9fd7154e910e3f64d53875b4bec492d9f67812b4
cristiaan3003 <cristiaan3003@gmail.com>
2020-05-12 10:30:41 -0300

    v.227-239 - streamApp - Handling form with Redux Form

--- streams_v199/client/src/components/streams/StreamCreate.js
+++ streams_v199/client/src/components/streams/StreamCreate.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
 
-const StreamCreate = () =>{
+class StreamCreate extends React.Component{
+
+    renderError = (meta)=> {
+        if(meta.touched && meta.error){
+            return (
+            <div className='ui error message'>
+                <div className='header'>
+                    {meta.error}
+                </div>
+            </div>
+            );}
+
+    }
+
+    renderInput = (formProps) =>{
+        //console.log(formProps);
+        return (
+        <div className='form'>
+            <label>  {formProps.label}    </label>
+            <input {...formProps.input} autoComplete='off'/>
+            {this.renderError(formProps.meta)}
+        </div>
+        
+            );
+    }
+
+    onSubmit(formValues){
+        //console.log(formValues);
+
+    }
+    render(){
     return (
         <React.Fragment>
-            <div>StreamCreate</div>
+            <form className='ui form error' onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                <Field name='title' component={this.renderInput} label='Enter Title'/>
+                <Field name='description' component={this.renderInput} label='Enter description'/>
+                <button className='ui button primary'>Submit</button>
+            </form>
         </React.Fragment>
-    );
-};
+    );}
+}
 
-export default StreamCreate;
+const validate = formValues =>{
+    const errors = {};
+    if(!formValues.title){
+        errors.title = 'You must enter title';
+    }
+    if (!formValues.description){
+        errors.description = 'You must enter description';
+    }
+    return errors;
+}
+
+export default reduxForm({
+    form: 'streamCreate',
+    validate: validate
+})(StreamCreate);
