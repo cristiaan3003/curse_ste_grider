8a20563c21c602f06bf60c0d128fca1f5ec740e0
cristiaan3003 <cristiaan3003@gmail.com>
2020-05-14 09:29:05 -0300

    v.275-282 - streamApp

--- streams_v199/client/src/components/streams/StreamDelete.js
+++ streams_v199/client/src/components/streams/StreamDelete.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Modal from '../Modal'
+import history from '../../history'
+
+
+const actions = (
+    <React.Fragment>
+        <button className="ui primary button">Delete</button>
+        <button className="ui button">Cancel</button>
+    </React.Fragment>
+);
 
 const StreamDelete = () =>{
     return (
         <React.Fragment>
             <div>StreamDelete</div>
+            <Modal
+            title='Delete Stream'
+            content= 'Are you shure to delete stream?'
+            actions= {actions}
+            onDismiss= {()=>history.push('/')}
+            ></Modal>
         </React.Fragment>
     );
 };
