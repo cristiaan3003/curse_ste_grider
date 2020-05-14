dad9bcf591f4eb56c78422b9c4b1fccf3a51fa4f
cristiaan3003 <cristiaan3003@gmail.com>
2020-05-14 10:25:46 -0300

    v.283-285 - streamApp

--- streams_v199/client/src/components/streams/StreamDelete.js
+++ streams_v199/client/src/components/streams/StreamDelete.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Modal from '../Modal'
+import {connect} from 'react-redux'
+import {fetchStream, deleteStream} from '../../actions'
+import {Link} from 'react-router-dom'
 import history from '../../history'
 
 
@@ -7,29 +10,44 @@ import history from '../../history'
 
 class StreamDelete extends React.Component{
 
+    componentDidMount(){
+        //console.log(this.props);
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
     renderActions(){        
             return(
                 <React.Fragment>
                     <button className="ui primary button">Delete</button>
-                    <button className="ui button">Cancel</button>
+                    <Link to='/' className="ui button">Cancel</Link>
                 </React.Fragment>
             );
     }
     
 
     render(){
+        console.log(this.props);
+    if (!this.props.stream){
+        return <div>Loading!!</div>
+    }
     return (
-        <React.Fragment>
-            <div>StreamDelete</div>
             <Modal
+            onClick={this.props.deleteStream(this.props.match.params.id)}
             title='Delete Stream'
-            content= 'Are you shure to delete stream?'
+            content= {`Are you shure to delete stream? ${this.props.stream.title}`}
             actions= {this.renderActions()}
             onDismiss= {()=>history.push('/')}
-            ></Modal>
-        </React.Fragment>
+            />
     );
     }
 }
 
-export default StreamDelete;
+const mapToStateToPros = (state, ownProps) =>{
+    //console.log('mapToState');
+    //console.log(state);
+    return {stream: state.streams[ownProps.match.params.id]};
+}
+export default connect(mapToStateToPros,{
+    fetchStream,
+    deleteStream
+})(StreamDelete);
