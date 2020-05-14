337a086413c80a47f83a30d5735b90ccf3ac8b62
cristiaan3003 <cristiaan3003@gmail.com>
2020-05-14 09:36:45 -0300

    v.283 - streamApp - refactor StreamDelete -- functional to class component

--- streams_v199/client/src/components/streams/StreamDelete.js
+++ streams_v199/client/src/components/streams/StreamDelete.js
@@ -3,25 +3,33 @@ import Modal from '../Modal'
 import history from '../../history'
 
 
-const actions = (
-    <React.Fragment>
-        <button className="ui primary button">Delete</button>
-        <button className="ui button">Cancel</button>
-    </React.Fragment>
-);
 
-const StreamDelete = () =>{
+
+class StreamDelete extends React.Component{
+
+    renderActions(){        
+            return(
+                <React.Fragment>
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </React.Fragment>
+            );
+    }
+    
+
+    render(){
     return (
         <React.Fragment>
             <div>StreamDelete</div>
             <Modal
             title='Delete Stream'
             content= 'Are you shure to delete stream?'
-            actions= {actions}
+            actions= {this.renderActions()}
             onDismiss= {()=>history.push('/')}
             ></Modal>
         </React.Fragment>
     );
-};
+    }
+}
 
 export default StreamDelete;
