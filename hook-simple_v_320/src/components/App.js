6536ef56667d7d67caad24df9fa98e6582081e3c
cristiaan3003 <cristiaan3003@gmail.com>
2020-06-11 09:31:10 -0300

    327. building resourse list

--- hook-simple_v_320/src/components/App.js
+++ hook-simple_v_320/src/components/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom'
+import ResourseList from './ResourseList'
 
 
 const App = () =>{
@@ -9,7 +10,7 @@ const App = () =>{
                 <button onClick={() => setResource('posts')}>posts</button>
                 <button onClick={() => setResource('todos')}>todos</button>
                 <br/>
-                {resourse}
+                <ResourseList resourse={resourse}></ResourseList>
                 
             </div>
         );
---
+++ hook-simple_v_320/src/components/ResourseList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class ResourseList extends React.Component{
+
+    render(){
+        return(
+            <div>
+                {this.props.resourse}
+            </div>
+        );
+    };
+}
+
+export default ResourseList;
